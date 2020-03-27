73eb63ebbc391360f95950224bc73019212617d0
1st-website <arnayan21@gmail.com>
2020-03-27 20:16:26 +0600

    Done

---
+++ null-undefined.js
@@ -0,0 +1,40 @@
+let pakhi ;
+console.log(pakhi);
+
+
+
+function add(num1 ,num2){
+    console.log(num1+num2);
+}
+const result = add (12,8);
+console.log(result);
+
+
+
+function add(num1 ,num2){
+    console.log(num1+num2);
+    return ;
+}
+const result = add (12,8);
+console.log(result);
+
+
+ function add(num1 ,num2){
+     console.log(num1 ,num2);
+     
+ }
+ const result = add (12);
+ console.log(result);
+
+
+
+const person = {name : "NAYAN",phone : 12345};
+console.log(person.id);
+
+
+let fun = undefined;
+console.log(fun);
+
+
+let ages =[2,3,4];
+console.log(ages[11]);
