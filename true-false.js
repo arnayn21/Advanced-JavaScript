5a42adac0932e0fdd844f68389a7aa50c3afbfbb
1st-website <arnayan21@gmail.com>
2020-03-27 18:11:11 +0600

    done

---
+++ true-false.js
@@ -0,0 +1,19 @@
+//falsy 
+//0
+//""
+//undefined
+//null
+//NaN
+
+//Truthy:
+//"0", " ",[],{}
+
+let name = 12;
+
+
+if(name || name == 0){
+    console.log("Condition is True");
+}
+else{
+    console.log("Condition is False");
+}
