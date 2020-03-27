120cf32eb20b182f9c2d983681f74d619b8d98a2
1st-website <arnayan21@gmail.com>
2020-03-27 20:40:24 +0600

    done

---
+++ double-vs ===equals.js
@@ -0,0 +1,43 @@
+const first = 2;
+const second =2;
+
+if(first==second){
+    console.log("condition is True");
+}
+else{
+    console.log("condition is False");
+}
+
+
+const first = 2;
+const second ="2";
+
+if(first===second){
+    console.log("condition is True");
+}
+else{
+    console.log("condition is False");
+}
+
+
+const first = 1;
+const second =true;
+
+if(first==second){
+    console.log("condition is True");
+}
+else{
+    console.log("condition is False");
+}
+
+
+
+const first = 0;
+const second =false;
+
+if(first==second){
+    console.log("condition is True");
+}
+else{
+    console.log("condition is False");
+}
